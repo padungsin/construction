c183ace39e3c1912f8ddec620b9afec30613214f
bpmsAdmin <>
2017-06-20 13:06:35 +0700

    {/construction/src/main/java/com/marlo/pms/management/MGTApplication.java}

--- construction/src/main/java/com/marlo/pms/management/MGTApplication.java
+++ construction/src/main/java/com/marlo/pms/management/MGTApplication.java
@@ -9,37 +9,37 @@ public class MGTApplication implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
-   private com.marlo.construction.management.MGTBusinessData businessData;
-   private com.marlo.construction.common.CommonProcessTask taskData;
+   private com.marlo.pms.management.MGTBusinessData businessData;
+   private com.marlo.pms.common.CommonProcessTask taskData;
 
    public MGTApplication()
    {
    }
 
-   public com.marlo.construction.management.MGTBusinessData getBusinessData()
+   public com.marlo.pms.management.MGTBusinessData getBusinessData()
    {
       return this.businessData;
    }
 
    public void setBusinessData(
-         com.marlo.construction.management.MGTBusinessData businessData)
+         com.marlo.pms.management.MGTBusinessData businessData)
    {
       this.businessData = businessData;
    }
 
-   public com.marlo.construction.common.CommonProcessTask getTaskData()
+   public com.marlo.pms.common.CommonProcessTask getTaskData()
    {
       return this.taskData;
    }
 
-   public void setTaskData(com.marlo.construction.common.CommonProcessTask taskData)
+   public void setTaskData(com.marlo.pms.common.CommonProcessTask taskData)
    {
       this.taskData = taskData;
    }
 
    public MGTApplication(
-         com.marlo.construction.management.MGTBusinessData businessData,
-         com.marlo.construction.common.CommonProcessTask taskData)
+         com.marlo.pms.management.MGTBusinessData businessData,
+         com.marlo.pms.common.CommonProcessTask taskData)
    {
       this.businessData = businessData;
       this.taskData = taskData;
