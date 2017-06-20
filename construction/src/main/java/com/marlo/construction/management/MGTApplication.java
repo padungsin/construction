7df9c3e71b634518fc352ca75879db0b6f98ff3f
bpmsAdmin <>
2017-06-20 09:28:32 +0700

    {/construction/src/main/java/com/marlo/construction/management/MGTApplication.java}

--- construction/src/main/java/com/marlo/construction/management/MGTApplication.java
+++ construction/src/main/java/com/marlo/construction/management/MGTApplication.java
@@ -9,8 +9,40 @@ public class MGTApplication implements java.io.Serializable
 
    static final long serialVersionUID = 1L;
 
+   private com.marlo.construction.management.MGTBusinessData businessData;
+   private com.marlo.construction.common.CommonProcessTask taskData;
+
    public MGTApplication()
    {
+   }
+
+   public com.marlo.construction.management.MGTBusinessData getBusinessData()
+   {
+      return this.businessData;
+   }
+
+   public void setBusinessData(
+         com.marlo.construction.management.MGTBusinessData businessData)
+   {
+      this.businessData = businessData;
+   }
+
+   public com.marlo.construction.common.CommonProcessTask getTaskData()
+   {
+      return this.taskData;
+   }
+
+   public void setTaskData(com.marlo.construction.common.CommonProcessTask taskData)
+   {
+      this.taskData = taskData;
+   }
+
+   public MGTApplication(
+         com.marlo.construction.management.MGTBusinessData businessData,
+         com.marlo.construction.common.CommonProcessTask taskData)
+   {
+      this.businessData = businessData;
+      this.taskData = taskData;
    }
 
 }
